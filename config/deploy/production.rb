cae0eb37a20da87c951b6bd1060122d15f7803d6
Neil Marion dela Cruz <neilmarion@freelancer.com>
2013-08-09 10:49:36 +0800

    Wrong symlink corrected

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -41,8 +41,8 @@ namespace :deploy do
   after "deploy:setup", "deploy:setup_config"
 
   task :symlink_config, roles: :app do
-    run "ln -nfs #{shared_path}/config/database.yml.production-sample #{release_path}/config/database.yml"
-    run "ln -nfs #{shared_path}/config/redis.yml.sample #{release_path}/config/redis.yml"
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/redis.yml #{release_path}/config/redis.yml"
   end 
   after "deploy:finalize_update", "deploy:symlink_config"
 
