7db5e6212439a8598a32fddf50a00aa38ceee296
Neil Marion dela Cruz <neilmarion@freelancer.com>
2013-08-09 10:06:43 +0800

    deploy

---
+++ config/deploy/production.rb
@@ -0,0 +1,101 @@
+# This is a set of sample deployment recipes for deploying via Capistrano.
+# One of the recipes (deploy:symlink_nginx) assumes you have an nginx configuration
+# file at config/nginx.conf. You can make this easily from the provided sample
+# nginx configuration file.
+#
+# For help deploying via Capistrano, see this thread:
+# http://meta.discourse.org/t/deploy-discourse-to-an-ubuntu-vps-using-capistrano/6353
+
+# Repo Settings
+# You should change this to your fork of discourse
+set :branch, fetch(:branch, 'master')
+set :scm, :git
+ssh_options[:forward_agent] = true
+
+# General Settings
+set :deploy_type, :deploy
+default_run_options[:pty] = true
+
+# Server Settings
+set :user, 'deployer'
+
+set :application, 'discourse'
+set :deploy_to, "/home/#{user}/apps/#{application}"
+
+set :rails_env, :production
+
+namespace :deploy do
+  %w[start stop restart].each do |command|
+    desc "restarting nginx"
+    task command, roles: :app, except: {no_release: true} do
+      sudo "service nginx restart" 
+    end 
+  end 
+
+  task :setup_config, roles: :app do
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    put File.read("config/facebook.example.yml"), "#{shared_path}/config/facebook.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end 
+  after "deploy:setup", "deploy:setup_config"
+
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml.production-sample #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/redis.yml.sample #{release_path}/config/redis.yml"
+  end 
+  after "deploy:finalize_update", "deploy:symlink_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end 
+  end 
+  before "deploy", "deploy:check_revision"
+end
+
+# Symlink config/nginx.conf to /etc/nginx/sites-enabled. Make sure to restart
+# nginx so that it picks up the configuration file.
+#namespace :config do
+#  task :nginx, roles: :app do
+#    puts "Symlinking your nginx configuration..."
+#    sudo "ln -nfs #{release_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+#  end
+#end
+
+#after "deploy:setup", "config:nginx"
+
+# Tasks to start/stop/restart a daemonized clockwork instance
+namespace :clockwork do
+  desc "Start clockwork"
+  task :start, :roles => [:app] do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec clockworkd -c #{current_path}/config/clock.rb --pid-dir #{shared_path}/pids --log --log-dir #{shared_path}/log start"
+  end
+
+  task :stop, :roles => [:app] do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec clockworkd -c #{current_path}/config/clock.rb --pid-dir #{shared_path}/pids --log --log-dir #{shared_path}/log stop"
+  end
+
+  task :restart, :roles => [:app] do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec clockworkd -c #{current_path}/config/clock.rb --pid-dir #{shared_path}/pids --log --log-dir #{shared_path}/log restart"
+  end
+end
+
+after  "deploy:stop",    "clockwork:stop"
+after  "deploy:start",   "clockwork:start"
+before "deploy:restart", "clockwork:restart"
+
+# Seed your database with the initial production image. Note that the production
+# image assumes an empty, unmigrated database.
+namespace :db do
+  desc 'Seed your database for the first time'
+  task :seed do
+    run "cd #{current_path} && psql -d discourse_production < pg_dumps/production-image.sql"
+  end
+end
+
+# Migrate the database with each deployment
+after  'deploy:update_code', 'deploy:migrate'
